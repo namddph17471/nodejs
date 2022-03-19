a2826161b30df018492cce59181299e7c3d3fb5c
namddph17471 <namddph17471@fpt.edu.vn>
2022-03-19 17:35:19 +0700

    update

--- routers/cateProduct.js
+++ routers/cateProduct.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { createCateProduct, deleteCateProduct, detailCateproduct, listCateProduct, upadteCateProduct } from "../controllers/cateProduct";
-import { updateProduct } from "../controllers/products";
 const cateProductRouter = express.Router();
 cateProductRouter.get("/cateProduct",listCateProduct);
 cateProductRouter.get("/cateProduct/:id",detailCateproduct);
