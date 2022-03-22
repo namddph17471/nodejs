7ffec4cd9c5b93a8c199e0e7f3882781545440cd
namddph17471 <namddph17471@fpt.edu.vn>
2022-03-22 17:54:10 +0700

    update

--- app.js
+++ app.js
@@ -13,27 +13,6 @@ app.use("/api",productRouter);
 app.use("/api",cateProductRouter)
 app.use(newRouter);
 mongoose.connect('mongodb://localhost:27017/web16307');
-// Khỏi tạo server
-// const server = http.createServer((request,respon)=>{
-//     if (request.url === "/") {
-//         // console.log("Home Page");
-//         respon.setHeader("Content-Type","text/html");// 
-//         respon.write("<h1>Home page </h1>");// hiện thị ra màn hình
-//         respon.end  //kết thúc
-//     };
-//     if (request.url === "/product") {
-//         // console.log("Product Page");
-//         respon.setHeader("Content-Type","text/html");
-//         respon.write("<h1>Product page</h1>");
-//         respon.end  //kết thúc
-//     };
-//     if (request.url === "/news") {
-//         // console.log("Product Page");
-//         respon.setHeader("Content-Type","text/html");
-//         respon.write("<h1>New page</h1>");
-//         respon.end  //kết thúc
-//     };
-// });
 //bước 3: lắng nghe cổng thực thi
 const port = 3001;
 app.listen(port,()=>{
