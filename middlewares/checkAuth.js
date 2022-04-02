250309634b1c7bea9d9e3b2aad5c526b34c6d26f
namddph17471 <namddph17471@fpt.edu.vn>
2022-04-02 17:05:19 +0700

    check Auth

---
+++ middlewares/checkAuth.js
@@ -0,0 +1,25 @@
+import expressJwt from 'express-jwt'
+
+export const requiredSignin = expressJwt({
+    secret:"123456",
+    algorithms:["HS256"],
+    requestProperty: "auth"
+})
+export const isAuth = (request,response,next)=>{
+    console.log(request.auth)
+    console.log(request.profile)
+    if (!(request.profile._id == request.auth._id)) {
+        return response.status(400).json({
+            message:"Bạn không có quyền truy cập"
+        })
+    }
+    next()
+}
+export const isAdmin = (request,response,next)=>{
+    if(request.profile.role == 0 ){
+        response.status(400).json({
+            message:"Bạn không phải Admin"
+        })
+    }
+    next()
+}
--- routers/product.js
+++ routers/product.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { createProduct, deleteProduct, listProduct, listProductDetail, updateProduct } from "../controllers/products";
 import { userById } from "../controllers/user";
+import { isAdmin, isAuth, requiredSignin } from "../middlewares/checkAuth";
 const productRouter = express.Router();
 
 productRouter.get("/products",listProduct)
 productRouter.get("/products/:id",listProductDetail)
-productRouter.post("/products/:userId",createProduct)
+productRouter.post("/products/:userId",requiredSignin,isAuth,isAdmin, createProduct)
 productRouter.delete("/products/:id",deleteProduct)
 productRouter.put("/products/:id",updateProduct)
 
