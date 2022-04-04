389ba717336cce1f3b8461c730744f1d17c50c48
namddph17471 <namddph17471@fpt.edu.vn>
2022-04-04 08:23:05 +0700

    update

--- routers/cateProduct.js
+++ routers/cateProduct.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { createCateProduct, deleteCateProduct, listdetailCateproduct, listCateProduct, upadteCateProduct } from "../controllers/cateProduct";
+import { userById } from "../controllers/user";
+import { isAdmin, isAuth, requiredSignin } from "../middlewares/checkAuth";
 const cateProductRouter = express.Router();
 cateProductRouter.get("/cateProduct",listCateProduct);
 cateProductRouter.get("/cateProduct/:id",listdetailCateproduct);
 cateProductRouter.put("/cateProduct/:id",upadteCateProduct);
 cateProductRouter.delete("/cateProduct/:id",deleteCateProduct);
-cateProductRouter.post("/cateProduct",createCateProduct);
+cateProductRouter.post("/cateProduct/:userId",requiredSignin,isAuth,isAdmin,createCateProduct);
+
+
+cateProductRouter.param("userId",userById)
 export default cateProductRouter
