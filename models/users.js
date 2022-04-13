cbbb35334d34224541f43f387e098e5b0c174ffa
namddph17471 <namddph17471@fpt.edu.vn>
2022-04-13 09:37:21 +0700

    update

--- models/users.js
+++ models/users.js
@@ -15,15 +15,12 @@ const userSchema= new mongoose.Schema({
     },
     image:{
         type:String,
-        required:true
     },
     phone:{
         type:String,
-        required:true
     },
     address:{
         type:String,
-        required:true
     },
     role:{
         type:Number,
